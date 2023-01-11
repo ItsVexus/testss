f91aa87db8c4a51f2f215c5a5032155d3e481709
ItsVexus <118904680+ItsVexus@users.noreply.github.com>
2023-01-11 20:55:03 +0700

    Update and rename .gitignore to .lua

---
+++ .lua
@@ -0,0 +1,33 @@
+local lc = gameGetService(Players).LocalPlayer 
+
+local group = 8856667
+
+local grouplink = https://www.roblox.com/groups/8856667/Honk#!/about
+
+if lcIsInGroup(group) then 
+      print(FuaiHubPremium)
+else
+      lckick(пашёл нафиг не вайтлистнут) 
+end
+local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/bloodball/-back-ups-for-libs/main/kav"))()
+local Window = Library.CreateLib("vexusHub", "Serpent")
+local Tab = Window:NewTab("Main")
+local Section = Tab:NewSection("Scripts")
+Section:NewButton("North Pole Teleport", "ButtonInfo", function()
+
+local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/bloodball/-back-ups-for-libs/main/kav"))()
+local Window = Library.CreateLib("AutoTrain", "Serpent")
+local Tab = Window:NewTab("Farm")
+local Section = Tab:NewSection("idk")
+Section:NewButton("farm", "farm", function()
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-279, 281, 1000)
+wait(0.5)
+game.Players.LocalPlayer.Character.UpperTorso.Waist:remove()
+wait(0.5)
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-363, 15735, -3)
+_G.on = 1
+while _G.on == 1 do task.wait()
+local args = {[1] = {[1] = "+FS6"}}
+game:GetService("ReplicatedStorage").RemoteEvent:FireServer(unpack(args))
+end
+end)
